e6fc4aa3b2a0867f852b19382b3e62f91b5bf624
Tyson Brown <tysonb@buypd.com>
2014-09-11 09:21:07 -0600

    add ruby version file

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,3 @@
+class DashboardsController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Shouter::Application.routes.draw do
   # use 'resource singular' for singleton resource
   # (meaning only one instance that is available globally)
   # singleton resource does not have an id
+  # url will be: /dashboard (singular)
+  # controller will be dashboards_controller (plural)
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
