49358514553b0f948b4bb2cdcefcf5563d9b61e1
tysonb <provobrown@gmail.com>
2014-09-30 16:53:43 -0600

    add photoshouts partial link

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,6 +1,6 @@
 class PhotoShout < ActiveRecord::Base
-  has_attached_file :image
-# has_attached_file :image, styles: {
-#   shout: "200X200>"
-# }
+# has_attached_file :image
+  has_attached_file :image, styles: {
+    shout: "200X200>"
+  }
 end
